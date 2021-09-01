907ca52647f75849c61be04fc588760dc264dc90
Abby Leary <al409220@ohio.edu>
2021-09-01 17:56:26 -0400

    initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Abby Leary
+ *        Date: 9/1/21
+ * Description: Lesson 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
